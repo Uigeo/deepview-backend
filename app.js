50a26d39f5a06427ad62b09bd0551b335db38789
Yaeger <mug4019@gmail.com>
2019-01-11 20:35:28 +0900

    chagne CORS setting

--- app.js
+++ app.js
@@ -44,13 +44,13 @@ app.use('/*', function(req, res, next) {
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({
+  origin : ['http://localhost:3000'],
+  methods : ['GET', 'POST'],
+  credentials : true
+}));
 
-// {
-//   origin : ['http://localhost:3000'],
-//   methods : ['GET', 'POST'],
-//   credentials : true
-// }
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
